069f5a0c723f0cdba32d7ee14a91768c579e5a66
backslash112 <yangcun@live.com>
2015-09-18 14:19:01 +0800

    set the title color of the button in the example code

--- Example/YCStateButton/ViewController.swift
+++ Example/YCStateButton/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         let StateButton1 = YCStateButton(frame: CGRectMake(30, 30, 0, 0))
         StateButton1.selectedStateBackgroundImage = UIImage(named: "tag_selected_bg")!
         StateButton1.unSelectedStateBackgroundImage = UIImage(named: "tag_unselected_bg")!
-        StateButton1.selectedStatusTitleColor = UIColor.greenColor()
+        StateButton1.selectedStatusTitleColor = UIColor(red: 230/255, green: 255/255, blue: 246/255, alpha: 1.0)
         StateButton1.unselectedStatusTitleColor = UIColor.grayColor()
         StateButton1.fontSize = 15
         StateButton1.setTitle("Superman", forState: UIControlState.Normal)
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         let StateButton2 = YCStateButton(frame: CGRectMake(30, 100, 0, 0))
         StateButton2.selectedStateBackgroundImage = UIImage(named: "tag_selected_bg")!
         StateButton2.unSelectedStateBackgroundImage = UIImage(named: "tag_unselected_bg")!
-        StateButton2.selectedStatusTitleColor = UIColor.greenColor()
+        StateButton2.selectedStatusTitleColor = UIColor(red: 230/255, green: 255/255, blue: 246/255, alpha: 1.0)
         StateButton2.unselectedStatusTitleColor = UIColor.grayColor()
         StateButton2.fontSize = 15
         StateButton2.setTitle("Captain America", forState: UIControlState.Normal)
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
         let StateButton3 = YCStateButton(frame: CGRectMake(30, 170, 0, 0))
         StateButton3.selectedStateBackgroundImage = UIImage(named: "tag_selected_bg")!
         StateButton3.unSelectedStateBackgroundImage = UIImage(named: "tag_unselected_bg")!
-        StateButton3.selectedStatusTitleColor = UIColor.greenColor()
+        StateButton3.selectedStatusTitleColor = UIColor(red: 230/255, green: 255/255, blue: 246/255, alpha: 1.0)
         StateButton3.unselectedStatusTitleColor = UIColor.grayColor()
         StateButton3.fontSize = 15
         StateButton3.setTitle("Superman and Captain America", forState: UIControlState.Normal)
